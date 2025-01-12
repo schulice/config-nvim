f0304d592938f9383c2900c809a669d0795ac425
chenzx <lyxx2413@outlook.com>
2025-01-12 19:48:41 +0800

    auto update

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -2,20 +2,22 @@ return {
   -- add theme
   {
     -- "navarasu/onedark.nvim",
+    "schulice/onedark.nvim",
     -- "Mofiqul/vscode.nvim",
     -- 'ellisonleao/gruvbox.nvim',
-    "folke/tokyonight.nvim",
+    -- "folke/tokyonight.nvim",
     -- "olimorris/onedarkpro.nvim",
     priority = 1000,
     config = true,
-    opts = {},
+    opts = {
+    },
   },
 
   -- Configure LazyVim to load gruvbox
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "tokyonight",
+      colorscheme = "onedark",
     },
   },
 }
